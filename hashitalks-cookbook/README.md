70410da88422bd6262623fd1d7d4fa8ac77540ef
Matt Ray <github@mattray.dev>
2020-02-20 23:00:25 +1100

    Chef cookbook

---
+++ hashitalks-cookbook/README.md
@@ -0,0 +1,4 @@
+# hashitalks
+
+TODO: Enter the cookbook description here.
+
---
+++ hashitalks-cookbook/metadata.rb
@@ -0,0 +1,21 @@
+name 'hashitalks'
+maintainer 'Matt Ray'
+maintainer_email 'matt@chef.io'
+license 'Apache-2.0'
+description 'Installs/Configures a simple webpage for HashiTalks 2020 demo'
+version '0.1.0'
+chef_version '>= 14.0'
+
+depends 'apache2', '~> 8.0.2'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/hashitalks/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/hashitalks'
